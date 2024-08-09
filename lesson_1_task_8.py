7c7fd4738b320bf695cbf75aa908cf0930e35a82
MariaMartya <134267305+MariaMartya@users.noreply.github.com>
2024-08-09 13:02:48 +0300

    Add files via upload

---
+++ lesson_1_task_8.py
@@ -0,0 +1,25 @@
+def fu(num):
+ print(num)
+
+num=8
+fu(num)
+num=8
+fu(num)
+num=0
+fu(num)
+num=0
+fu(num)
+num=5
+fu(num)
+num=5
+fu(num)
+num=5
+fu(num)
+num=3
+fu(num)
+num=5
+fu(num)
+num=3
+fu(num)
+num=5
+fu(num)
